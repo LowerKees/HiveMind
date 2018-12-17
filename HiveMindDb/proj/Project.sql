cb1dfda4ff07f8dfaaf12cf28501e7ddcf369f15
martijn.beenker <martijn.beenker@avanade.com>
2018-12-17 19:53:32 +0100

    Toevoegen van tabel voor projecten

---
+++ HiveMindDb/proj/Project.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [proj].[Project]
+(
+	[ProjectId] INT NOT NULL PRIMARY KEY,
+	[ProjectName] NVARCHAR(128) NOT NULL,
+	[ProjectSummary] NVARCHAR(500) NOT NULL,
+	[LanguagePrimary] NVARCHAR(128) NULL,
+	[LanguageSecondary] NVARCHAR(128) NULL,
+	[LanguageTertiary] NVARCHAR(128) NULL,
+	[GitHubUrl] NVARCHAR(128) NULL
+)
--- HiveMindDb/proj/Projects.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Projects]
-(
-	[ProjectId] INT NOT NULL PRIMARY KEY,
-	[ProjectName] NVARCHAR(128) NOT NULL,
-	[ProjectLanguagePrimary] NVARCHAR(128) NULL,
-	[ProjectLanguageSecondary] NVARCHAR(128) NULL,
-	[ProjectLanguageTertiary] NVARCHAR(128) NULL,
-	[GitHubUrl] NVARCHAR(1000) NULL
-);

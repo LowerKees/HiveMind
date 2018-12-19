26be270963a21116180490aa291c714e85032c70
martijn.beenker <martijn.beenker@avanade.com>
2018-12-19 08:35:51 +0100

    Isolating CAST logic for hashing function

--- HiveMindDb/Utils/Stored Procedures/HashAttributes.sql
+++ HiveMindDb/Utils/Stored Procedures/HashAttributes.sql
@@ -4,7 +4,11 @@
 	@ignore_columns NVARCHAR(1000)
 AS
 	BEGIN
-		-- Determine column list
+		DECLARE
+			@msg NVARCHAR(128);			
+
+		-- Isolate neccessary metadata
+		DROP TABLE IF EXISTS #ColumnMetadata;
 		SELECT ColumName = cols.name 
 			, ColumnDataType = typ.name
 			, ColumnPrecision = cols.precision
@@ -13,6 +17,7 @@ AS
 			, IsPrimaryKey = CASE idxcols.is_included_column WHEN 0 THEN 1 ELSE 0 END
 			, IsIncludedColumn = ISNULL(idxcols.is_included_column, 0)
 			, OrdinalPosition = cols.column_id
+		INTO #ColumnMetadata
 		FROM sys.columns AS cols
 		JOIN sys.systypes AS typ 
 			ON cols.system_type_id = typ.xusertype
@@ -29,5 +34,55 @@ AS
 		WHERE OBJECT_NAME(tabs.OBJECT_ID) = 'Project' -- TODO: replace with parameter value
 		  AND sch.name = 'Proj' -- TODO: replace with parameter value
 		  AND idx.is_primary_key = 1
+		  AND cols.name != 'AttributeHash';
 
+		-- Cast data types to string
+		DROP TABLE IF EXISTS #CastedData;
+		SELECT CastedColumns =  
+			CASE 
+				-- do not transfor string data types
+				WHEN src.ColumnDataType IN ('NVARCHAR','VARCHAR','NCHAR','CHAR','NTEXT','TEXT') THEN src.ColumName
+				-- cast integers to nvarchar(20)
+				WHEN src.ColumnDataType IN ('TINYINT','SMALLINT','INT','BIGINT') THEN CONCAT('CAST(', src.ColumnName, ' AS NVARCHAR(20))')
+				-- cast date data type to yyyymmdd
+				WHEN src.ColumnDataType = 'DATE' THEN CONCAT('CONVERT(NVARCHAR(8), ', src.ColumnName, ', 112)')
+				-- cast datetime data types to yyyymmdd hh:mi:ss.mmmm
+				WHEN src.ColumnDataType IN ('DATETIME','DATETIME2') THEN CONCAT('CONVERT(NVARCHAR(23), ', src.ColumnName, ', 112)')
+				-- cast numerics and decimals to corresponding string
+				WHEN src.ColumnDataType IN ('DECIMAL','NUMERIC') THEN CONCAT('CAST(', src.ColumnName, ' AS NVARCHAR(39))')
+				-- cast floats and reals to corresponding string
+				WHEN src.ColumnDataType IN ('float','real') THEN CONCAT('CAST(', src.ColumName, ' AS NVARCHAR(54))')
+				ELSE NULL
+			END
+		INTO #CastedData
+		FROM #ColumnMetadata AS src;
+
+		BEGIN TRY
+			-- Perform checks on casted data
+			IF EXISTS (
+				SELECT *
+				FROM #CastedData
+				WHERE CastedColumns IS NULL
+				)
+				BEGIN
+					SET @msg = N'Data type from table cannot be hashed.';
+					THROW 50000, @msg, 1;
+				END
+		END TRY
+		BEGIN CATCH
+			DECLARE
+				@ErrorNumber INT,
+				@ErrorState INT,
+				@ErrorMessage NVARCHAR(1000),
+				@ErrorLine INT;
+			SELECT
+				@ErrorMessage = ERROR_MESSAGE()
+				, @ErrorNumber = ERROR_NUMBER()
+				, @ErrorState = ERROR_STATE()
+				, @ErrorLine = ERROR_LINE();
+
+			PRINT N'Hashing function did not complete properly. Please check previous error messages.';
+			PRINT CONCAT(N'Error found at line ', CAST(@ErrorLine AS NVARCHAR(100)));
+			THROW @ErrorNumber, @ErrorMessage, @ErrorState;
+		END CATCH
 	END
